7cd8189a0bc92841b4960cea70cd70ccc2bd3a7d
Jackson Kaplan <JwyattK@gmail.com>
2024-11-15 11:52:14 +0100

    add source files and swap to czmq

---
+++ src/socomm/socomm.h
@@ -0,0 +1,15 @@
+/**
+ * @file socomm.h
+ * @author Jackson Kaplan (JwyattK@gmail.com)
+ * @date 2024-11-14
+ * Copyright (c) 2024. All rights reserved.
+ */
+
+#ifndef SOCOMM_SOCOMM_H_
+#define SOCOMM_SOCOMM_H_
+
+namespace socomm {
+void test_function();
+}
+
+#endif
---
+++ test/main.test.cpp
@@ -0,0 +1,5 @@
+#include "socomm/socomm.h"
+
+int main()
+{
+}
