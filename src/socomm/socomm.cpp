9036f586ce277abdc13c20d84b766d973eb889f7
Jackson Kaplan <JwyattK@gmail.com>
2024-11-15 21:02:50 +0100

    beautiful example

--- src/socomm/socomm.cpp
+++ src/socomm/socomm.cpp
@@ -7,51 +7,81 @@
 
 #include "socomm/socomm.h"
 #include <cstdlib>
+#include <thread>
+#include <unistd.h>
+#include <vector>
 
 namespace socomm {
 
-node::node()
+inline void handle_status(int status)
 {
-  context_   = zmq_ctx_new();
-  void *ctx2 = zmq_ctx_new();
-  printf("Got Context\n");
-
-  socket_    = zmq_socket(context_, ZMQ_REP);
-  int status = zmq_bind(socket_, "tcp://*:9350");
-
   if (status != 0) {
+    printf("ERROR: %s\n", zmq_strerror(status));
     exit(status);
   }
+}
 
-  void *client_socket = zmq_socket(ctx2, ZMQ_REQ);
-  status              = zmq_connect(client_socket, "tcp://localhost:9350");
-  if (status != 0) {
-    exit(status);
+inline void handle_errno(int status)
+{
+  if (status == -1) {
+    printf("ERROR: %s\n", strerror(errno));
+    exit(errno);
   }
-  printf("Connected\n");
+}
 
-  char buff[12];
-  printf("Socket send\n");
-  status = zmq_send(socket_, "Thank you", 9, 0);
-  if (status != 0) {
-    printf("%s\n", zmq_strerror(status));
-    exit(status);
-  }
-  printf("Client receive\n");
-  zmq_recv(socket_, buff, 12, 0);
-  printf("Client socket receieved request: %s\n", buff);
+static void client_thread()
+{
+  int   status;
+
+  void *ctx = zmq_ctx_new();
+  sleep(10);
+
+  void *client_socket = zmq_socket(ctx, ZMQ_REQ);
+  status              = zmq_connect(client_socket, "tcp://localhost:5555");
+  handle_status(status);
+
+  std::vector<char> buff(128);
 
   printf("Client send\n");
   status = zmq_send(client_socket, "Please", 6, 0);
-  if (status != 0) {
-    exit(status);
-  }
-  printf("Socket receive\n");
-  zmq_recv(socket_, buff, 12, 0);
-  printf("Server socket receieved request: %s\n", buff);
+  handle_errno(status);
+
+  printf("Client receive\n");
+  status = zmq_recv(client_socket, buff.data(), buff.size(), 0);
+  handle_errno(status);
+
+  printf("Client socket receieved request: %s\n", buff.data());
 
   zmq_close(client_socket);
-  zmq_ctx_destroy(ctx2);
+}
+
+node::node()
+{
+  printf("Begin\n");
+  context_ = zmq_ctx_new();
+  printf("Got Context\n");
+
+  int status;
+
+  socket_ = zmq_socket(context_, ZMQ_REP);
+  status  = zmq_bind(socket_, "tcp://*:5555");
+  handle_status(status);
+
+  std::thread       t(client_thread);
+
+  std::vector<char> buff(128);
+
+  printf("Socket receive\n");
+  status = zmq_recv(socket_, buff.data(), buff.size(), 0);
+  handle_errno(status);
+
+  printf("Server socket receieved request: %s\n", buff.data());
+
+  printf("Socket send\n");
+  status = zmq_send(socket_, "Thank you", 9, 0);
+  handle_errno(status);
+
+  t.join();
 }
 
 node::~node()
