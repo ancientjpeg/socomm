ae61af41619d9d29d1a46f8b9d12a26c990d3c35
Jackson Kaplan <JwyattK@gmail.com>
2025-02-01 15:42:14 -0800

    begin adding test

---
+++ test/ledger.test.c
@@ -0,0 +1,10 @@
+#include <socomm/ledger.h>
+
+int main()
+{
+  socomm_ledger_t *ledger        = socomm_ledger_create();
+
+  const int        TEST_ARR_SIZE = 5;
+  uuid4_t          uuids[TEST_ARR_SIZE];
+  uint16_t         ports[TEST_ARR_SIZE];
+}
