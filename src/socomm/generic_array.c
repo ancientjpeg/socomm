bb19654f5daa289f5dcbd5d6982a102ad1600f11
Jackson Kaplan <JwyattK@gmail.com>
2025-02-01 16:56:11 -0800

    add generic array files

---
+++ src/socomm/generic_array.c
@@ -0,0 +1,8 @@
+/**
+ * @file generic_array.h
+ * @author Jackson Kaplan (JwyattK@gmail.com)
+ * @date 2025-02-01
+ * Copyright (c) 2025. All rights reserved.
+ */
+
+#include "generic_array.h"
---
+++ src/socomm/generic_array.h
@@ -0,0 +1,11 @@
+/**
+ * @file generic_array.h
+ * @author Jackson Kaplan (JwyattK@gmail.com)
+ * @date 2025-02-01
+ * Copyright (c) 2025. All rights reserved.
+ */
+
+#ifndef SOCOMM_GENERIC_ARRAY_H_
+#define SOCOMM_GENERIC_ARRAY_H_
+
+#endif
