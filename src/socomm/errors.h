a94497a9cda114e45203b82893bcff7536b0876f
Jackson Kaplan <JwyattK@gmail.com>
2025-02-03 20:22:33 -0800

    add generic error

--- src/socomm/errors.h
+++ src/socomm/errors.h
@@ -14,9 +14,10 @@
  */
 typedef enum socomm_error_t {
   SOCOMM_SUCCESS        = 0,
-  SOCOMM_ALREADY_EXISTS = -1,
-  SOCOMM_DOES_NOT_EXIST = -2,
-  SOCOMM_PORT_IN_USE    = -3,
+  SOCOMM_ERROR          = -1,
+  SOCOMM_ALREADY_EXISTS = -2,
+  SOCOMM_DOES_NOT_EXIST = -3,
+  SOCOMM_PORT_IN_USE    = -4,
 } socomm_error_t;
 
 static_assert(SOCOMM_SUCCESS == 0, "SOCOMM_SUCCESS is guaranteed to be 0.");
