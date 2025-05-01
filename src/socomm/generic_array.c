5160afdf4e3a8b8a7f679bee1bb10ca32e8b32a6
Jackson Kaplan <JwyattK@gmail.com>
2025-04-30 19:08:46 -0700

    refactor after all this while

--- src/socomm/generic_array.c
+++ src/socomm/generic_array.c
@@ -15,13 +15,19 @@
 #define SOCOMM_ARRAY_ELEMENT_STATIC_SIZE_MAX 16
 #endif
 
+static void socomm_verify_comparator(socomm_array_comp_t *comp)
+{
+  if (*comp == NULL) {
+    *comp = memcmp;
+  }
+}
+
 typedef struct socomm_array_t {
   void               *data;
   size_t              element_size;
   size_t              len;
   size_t              cap;
   socomm_array_dtor_t dtor;
-  socomm_array_comp_t comp;
 } socomm_array;
 
 socomm_array *socomm_array_create(size_t element_size)
@@ -31,7 +37,10 @@ socomm_array *socomm_array_create(size_t element_size)
 
 socomm_array *socomm_array_create_reserve(size_t element_size, size_t reserve)
 {
-  assert(element_size != 0);
+
+  if (element_size == 0) {
+    return NULL;
+  }
 
   socomm_array *array = malloc(sizeof(socomm_array));
 
@@ -40,11 +49,8 @@ socomm_array *socomm_array_create_reserve(size_t element_size, size_t reserve)
   array->len          = 0;
   array->cap          = 0;
   array->dtor         = NULL;
-  array->comp         = memcmp;
 
   socomm_array_reserve(array, reserve);
-
-  assert(array->cap >= reserve);
 
   return array;
 }
@@ -71,14 +77,8 @@ void socomm_array_set_dtor(socomm_array *array, socomm_array_dtor_t dtor)
   array->dtor = dtor;
 }
 
-void socomm_array_set_comp(socomm_array *array, socomm_array_comp_t comp)
-{
-  array->comp = comp;
-}
-
 void socomm_array_reserve(socomm_array *array, size_t reserve)
 {
-
   if (array->cap >= reserve) {
     return;
   }
@@ -139,19 +139,31 @@ void *socomm_array_insert_at(socomm_array *array, void *element, size_t index)
 
 void *socomm_array_at(socomm_array *array, size_t index)
 {
-  assert(index <= array->len);
   return array->data + (index * array->element_size);
 }
 
-void *socomm_array_find(socomm_array *array, void *element)
+size_t socomm_array_index_of(socomm_array       *array,
+                             void               *element,
+                             socomm_array_comp_t comp)
 {
+  void *found = socomm_array_find(array, element, comp);
+  return found == NULL ? socomm_array_length(array)
+                       : (found - array->data) / array->element_size;
+}
+
+void *
+socomm_array_find(socomm_array *array, void *element, socomm_array_comp_t comp)
+{
+
+  socomm_verify_comparator(&comp);
+
   const bool static_element_size
       = array->element_size < SOCOMM_ARRAY_ELEMENT_STATIC_SIZE_MAX;
 
   for (size_t i = 0; i < array->len; ++i) {
     size_t data_offset = i * array->element_size;
     void  *cmp_data    = array->data + data_offset;
-    if (array->comp(element, cmp_data, array->element_size) == 0) {
+    if (comp(element, cmp_data, array->element_size) == 0) {
       return cmp_data;
     }
   }
@@ -159,42 +171,44 @@ void *socomm_array_find(socomm_array *array, void *element)
   return NULL;
 }
 
-bool socomm_array_contains(socomm_array *array, void *element)
+bool socomm_array_contains(socomm_array       *array,
+                           void               *element,
+                           socomm_array_comp_t comp)
 {
-  return socomm_array_find(array, element) != NULL;
+  return socomm_array_find(array, element, comp) != NULL;
 }
 
-void socomm_array_pop_back(socomm_array *array)
+bool socomm_array_pop_back(socomm_array *array)
 {
   if (socomm_array_length(array) == 0) {
-    return;
+    return false;
   }
 
-  socomm_array_remove(array, array->len - 1);
+  return socomm_array_remove(array, array->len - 1);
 }
-void socomm_array_remove(socomm_array *array, size_t index)
+
+bool socomm_array_remove(socomm_array *array, size_t index)
 {
-  if (socomm_array_length(array) == 0) {
-    return;
+  if (index >= array->len) {
+    return false;
   }
 
-  uint8_t *target_element = (uint8_t *)socomm_array_at(array, index);
-  if (target_element == NULL) {
-    return;
-  }
-
+  uint8_t *target_element     = (uint8_t *)socomm_array_at(array, index);
   uint8_t *target_replacement = target_element + array->element_size;
-  size_t   data_size          = array->len * array->element_size;
-  uint8_t *end                = array->data + data_size;
-  size_t   data_move_size     = end - target_replacement;
+  uint8_t *end            = socomm_array_at(array, socomm_array_length(array));
+  size_t   data_move_size = end - target_replacement;
 
   if (array->dtor != NULL) {
     array->dtor(target_element, array->element_size);
   }
 
-  memmove(target_element, target_replacement, data_move_size);
+  if (target_element != end) {
+    memmove(target_element, target_replacement, data_move_size);
+  }
 
   --array->len;
+
+  return true;
 }
 
 void socomm_array_element_swap(void *a, void *b, size_t size)
@@ -220,26 +234,42 @@ void socomm_array_element_swap(void *a, void *b, size_t size)
   }
 }
 
-void socomm_array_purge(socomm_array *array, void *element)
+size_t
+socomm_array_purge(socomm_array *array, void *element, socomm_array_comp_t comp)
 {
 
-  void *entry;
+  socomm_verify_comparator(&comp);
 
   /**
-   * yes, jackson, I know this can overflow, but if it does the
+   * Yes, Jackson, I know this can underflow, but if it does the
    * loop won't run. Calm down.
-   * - jackson
+   * - Jackson
    */
-  void *swap_target
-      = (uint8_t *)(array->data) + array->len * array->element_size;
+  uint8_t *swap_target
+      = (uint8_t *)socomm_array_at(array, socomm_array_length(array) - 1);
+  size_t removed = 0;
 
-  for (size_t i = 0; i < array->len; ++i) {
-    void *current_element
-        = (void *)((uint8_t *)array->data + (i * array->element_size));
-    if (array->comp(current_element, element, array->element_size) == 0) {
+  for (size_t i = 0; i < socomm_array_length(array);) {
+
+    void *current_element = socomm_array_at(array, i);
+
+    if (comp(current_element, element, array->element_size) == 0) {
+      socomm_array_element_swap(current_element, element, array->element_size);
+      socomm_array_pop_back(array);
+      ++removed;
+
+      /**
+       * @todo I'm not 100% confident this won't underflow. My gut says it
+       * won't, but write out a proof to be sure.
+       */
+      swap_target -= array->element_size;
+      continue;
     }
-    swap_target -= array->element_size;
+
+    ++i;
   }
+
+  return removed;
 }
 
 void *socomm_array_element_at(socomm_array *array, size_t index)
--- src/socomm/generic_array.h
+++ src/socomm/generic_array.h
@@ -25,7 +25,6 @@ socomm_array *socomm_array_create_reserve(size_t element_size, size_t reserve);
 void          socomm_array_destroy(socomm_array **array);
 
 void   socomm_array_set_dtor(socomm_array *array, socomm_array_dtor_t dtor);
-void   socomm_array_set_comp(socomm_array *array, socomm_array_comp_t comp);
 
 void   socomm_array_reserve(socomm_array *array, size_t reserve);
 
@@ -65,23 +64,63 @@ void  *socomm_array_insert_at(socomm_array *array, void *element, size_t index);
  *
  * @param array
  * @param index
+ * @return The pointer to element `index` of `array`.
  */
 void  *socomm_array_at(socomm_array *array, size_t index);
 
-void  *socomm_array_find(socomm_array *array, void *element);
-bool   socomm_array_contains(socomm_array *array, void *element);
-
-void   socomm_array_pop_back(socomm_array *array);
-void   socomm_array_remove(socomm_array *array, size_t index);
+/**
+ * @brief Returns the index of `element` in `array` if it is found using
+ * `comp` as a comparator. If the element is not found, returns
+ * `socomm_array_length(array)`.
+ */
+size_t socomm_array_index_of(socomm_array       *array,
+                             void               *element,
+                             socomm_array_comp_t comp);
 
 /**
- * @brief Remove all elements where the result of the `socomm_array_comparator`
- * check against `element` is 0.
+ * @brief Finds the first element in `array` that matches `element` and returns
+ * it.
  *
  * @param array
- * @param element
+ * @param element The element to find.
+ * @param comp The comparator function. If `NULL`, `memcmp` is used.
+ * @return The found element, or NULL if no matching element was found.
  */
-void   socomm_array_purge(socomm_array *array, void *element);
+void *
+socomm_array_find(socomm_array *array, void *element, socomm_array_comp_t comp);
+
+/**
+ * @brief `true` if `array` contains `element` when compared using `comp`.
+ */
+bool   socomm_array_contains(socomm_array       *array,
+                             void               *element,
+                             socomm_array_comp_t comp);
+
+/**
+ * @brief Removes the last element of `array`. Returns `false` if array
+ * was already empty or the removal fails for some other reason.
+ */
+bool   socomm_array_pop_back(socomm_array *array);
+
+/**
+ * @brief Removes the element at `index`. Maintains stable ordering of
+ * other array elements. Returns `false` if `index` is out-of-bounds.
+ */
+bool   socomm_array_remove(socomm_array *array, size_t index);
+
+/**
+ * @brief Remove all elements equal to `element`. Is not stable; not
+ * guaranteed to maintain the ordering of `array`, but is guaranteed
+ * to perform at most O(n) swaps.
+ *
+ * @param array
+ * @param element The element to delete.
+ * @param comp The comparator function. If `NULL`, `memcmp` is used.
+ * @return The number of elements removed.
+ */
+size_t socomm_array_purge(socomm_array       *array,
+                          void               *element,
+                          socomm_array_comp_t comp);
 
 void  *socomm_array_element_at(socomm_array *array, size_t index);
 void  *socomm_array_element_at_checked(socomm_array *array, size_t index);
